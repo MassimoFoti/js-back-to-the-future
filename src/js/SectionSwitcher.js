3eedb5610732cbeceeeb69554af8b43779fc6ad3
Massimo Foti <massimo.foti@appway.com>
2017-10-26 12:25:38 +0200

    SectionSwitcher and RSS feed

--- src/js/SectionSwitcher.js
+++ src/js/SectionSwitcher.js
@@ -6,20 +6,41 @@
 	jsBack.SectionSwitcher = function(options){
 
 		var config = {
-			sectionSelector: "section"
+			sectionSelector: "section",
+			defaultSectionId: undefined
 		};
 		luga.merge(config, options);
 
+		var self = this;
+
+		var sectionsMap = {};
+
 		var init = function(){
-
+			populateMap();
 		};
 
-		this.displaySection = function(sectionId) {
-			console.debug("displaySection: " + sectionId)
+		var populateMap = function(){
+			jQuery(config.sectionSelector).each(function(index){
+				var jSection = $(this);
+				sectionsMap[jSection.attr("id")] =  jSection;
+			});
 		};
 
-		this.displayDefault = function() {
-			console.debug("displayDefault")
+		var hideAll = function(){
+			jQuery(config.sectionSelector).hide();
+		};
+
+		this.displaySection = function(sectionId){
+			var targetSection = sectionsMap[sectionId];
+			// Fail silently if we don't recognize the given id
+			if(targetSection !== undefined) {
+				hideAll();
+				targetSection.show();
+			}
+		};
+
+		this.displayDefault = function(){
+			self.displaySection(config.defaultSectionId);
 		};
 
 		init();
--- src/js/app.js
+++ src/js/app.js
@@ -7,11 +7,35 @@
 		alert("The demo is not going to work properly if accessed from a file system. You should use an HTTP server instead");
 	}
 
+	var feedDateFormatter = function(row, rowIndex, dataSet){
+		var dateObj = new Date(row.pubDate);
+		// Turn date into a reasonable string rapresentation
+		// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/toUTCString
+		var dateStr = dateObj.toUTCString();
+		var trimmedStr = dateStr.substring(0, dateStr.length - 12);
+		row.pubDate = trimmedStr;
+		return row;
+	};
+
+	var rssDs = new luga.data.Rss2Dataset({
+		uuid: "feedDs",
+		url: "feed/heshootshescoores.com.xml",
+		formatter: feedDateFormatter
+	});
+
 	jQuery(document).ready(function(){
-		var switcher = new jsBack.SectionSwitcher({});
+
+		rssDs.loadData();
+
+		var switcher = new jsBack.SectionSwitcher({
+			sectionSelector: "#content section",
+			defaultSectionId: "home"
+		});
+
 		new jsBack.Controller({
 			sectionSwitcher: switcher
 		});
+
 	});
 
 }());
