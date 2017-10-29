c981cfedc5671406c1c43b64f078cde88f560edc
Massimo Foti <massimo.foti@appway.com>
2017-10-29 11:33:11 +0100

    Updated gulp.file for ESLint

--- test/gulpfile.js
+++ test/gulpfile.js
@@ -1,9 +1,15 @@
+/* eslint no-implicit-globals: "off" */
+/* eslint strict: "off" */
+/* global require, __dirname */
+
+"use strict";
+
 var gulp = require("gulp");
 var karmaServer = require("karma").Server;
 
 /* Tasks */
 
-gulp.task("coverage", function (done) {
+gulp.task("coverage", function(done){
 	// Use Karma only for the sake of producing a code coverage report
 	new karmaServer({
 		configFile: __dirname + "/karma.conf.js"
