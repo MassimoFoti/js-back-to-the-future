74490cc2474301db08f770a4bf31af39dfd0aa48
Massimo Foti <massimo.foti@appway.com>
2017-12-23 23:12:38 +0100

    Removed redundant dataset

--- src/js/app.js
+++ src/js/app.js
@@ -8,20 +8,13 @@
 		alert("The demo is not going to work properly if accessed from a file system. You should use an HTTP server instead");
 	}
 
-	var rssDataSet = new luga.data.Rss2Dataset({
-		uuid: "feedDs",
-		url: "feed/heshootshescoores.com.xml",
-		formatter: jsBack.utils.feedDateFormatter
-	});
-
 	var libsDataSet = new luga.data.JsonDataSet({
 		uuid: "libsDs",
 		url: "data/libraries.json"
 	});
 
 	jQuery(document).ready(function(){
-
-		rssDataSet.loadData();
+		
 		libsDataSet.loadData();
 
 		var switcher = new jsBack.SectionSwitcher({
